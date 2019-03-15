de63dfc65ee62781bf0be2cb90b7697321624bff
ujjawalsaini3 <35144881+ujjawalsaini3@users.noreply.github.com>
2019-03-15 17:24:07 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ echo "command_not_found_handle() {
 }
 PS1='\033[1;91mroot@termux[\033[1;93m\W\033[1;91m]:#
 \033[1;92m'
-figlet $piedy" > bash.bashrc
+figlet FemurTech" > bash.bashrc
 cd /$HOME
 cd
 cd termuxstyling

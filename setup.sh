d03f6724d2f504badd80a06ce0e2ee571777ef1c
ujjawalsaini3 <35144881+ujjawalsaini3@users.noreply.github.com>
2019-03-24 15:32:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,51 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo Script made by:- Dark Warrior
+if [ ! -e "DW.config" ]
+then
+	read -p "Type your username: " uname </dev/tty
+	echo "This is your username: $uname"
+	echo "$uname" > DW.config
+else
+	read -p "Would You Like to Change Your Username[Y/N]: " ink
+	case "$ink" in
+		[yY][eE][sS]|[yY])
+	rm DW.config
+	clear
+	sh hi.sh
+	;;
+	*)
+	uname=$(cat DW.config)
+	echo "Welcome back $uname"
+	;;
+	esac
+fi
 cd
 cd /$HOME
 apt update
 apt upgrade
 apt install figlet toilet
+pkg install ncurses-utils
 cd ..
 cd usr/etc
 rm motd
+if [ -z $uname ]
+then
+	uname="FemurTech"
+fi
 echo "command_not_found_handle() {
-        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "'$1'"
 }
+figlet $uname
 PS1='\033[1;91mroot@termux[\033[1;93m\W\033[1;91m]:#
 \033[1;92m'
-figlet FemurTech" > bash.bashrc
-cd /$HOME
+cd /sdcard
+alias clear=\"clear;bash\"" > bash.bashrc
+cd /$HOM
 cd
 cd termuxstyling
 cat README.md
-figlet Dark
-figlet Warrior
+toilet Dark
+toilet Warrior
 echo Script made by Dark Warrior
 echo subscribe our youtube channel FemurTech
 echo tinyurl.com/femurtech

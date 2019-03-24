af9a4ddf69af39a533260fbac9f6aad8df20f095
xenoCoder01 <48878137+xenoCoder01@users.noreply.github.com>
2019-03-24 21:24:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ fi
 echo "command_not_found_handle() {
         /data/data/com.termux/files/usr/libexec/termux/command-not-found "'$1'"
 }
-shell() { sh \$1.sh;bash \$1.bash }
+shell() { sh \$1.sh;bash \$1.bash; }
 txt() { cat \$1.*; }
 figlet $uname
 PS1='\033[1;91mroot@termux[\033[1;93m\W\033[1;91m]:#

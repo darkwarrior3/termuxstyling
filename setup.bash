5f6523af16c1eba579a553923534243d9409fb9d
xenocode01 <spamtemp03@gmail>
2019-03-26 16:27:42 +0530

    new

---
+++ setup.bash
@@ -0,0 +1,213 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Setting up commands
+spinner()
+{
+	PID=$!
+	tput civis
+	echo -n "Installing "
+	# While process is running...
+	while kill -0 $PID 2> /dev/null;
+	do  
+		printf '\u2588'
+    	sleep 1
+	done
+printf " Done!\n"
+}
+echo Script made by:- Dark Warrior
+#Uninstall
+if [ -e "user.cfg" ]
+then
+	uname=$(sed '1q;d' user.cfg)
+	istate=$(sed '2q;d' user.cfg)
+	if [ "$istate" -eq "1" ]
+	then
+		read -p "Uninstall? [Y/N]: " ink1
+		case $ink1 in
+			[yY][eE][sS]|[yY])
+		rm user.cfg;
+		echo $uname > user.cfg
+		echo 0 >> user.cfg
+		cd
+		cd /$HOME
+		cd ..
+		cd usr/etc
+		echo "command_not_found_handle() {
+        	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+		} 
+		PS1='\$ '" > bash.bashrc
+		echo "Welcome to Termux!
+		Wiki:            https://wiki.termux.com
+		Community forum: https://termux.com/community
+		IRC channel:     #termux on freenode
+		Gitter chat:     https://gitter.im/termux/termux
+		Mailing list:    termux+subscribe@groups.io
+
+		Search packages:   pkg search <query>
+		Install a package: pkg install <package>
+		Upgrade packages:  pkg upgrade
+		Learn more:        pkg help" > motd
+		cd
+		cd /$HOME
+		figlet FemurTech
+		echo Now Your Termux is back to Original
+		echo "Uninstalled Succesfully"
+		return
+		;;
+			*)
+		;;
+		esac
+	fi
+fi
+#Assigns Username
+if [ ! -e "user.cfg" ] 
+then
+	read -p "Type your username: " uname </dev/tty
+	echo "This is your username: $uname"
+	echo "$uname" > user.cfg
+	echo "1" >> user.cfg
+#Rename Username
+else
+	rm user.cfg
+	echo "$uname" > user.cfg
+	echo "1" >> user.cfg
+	read -p "Would You Like to Change Your Username[Y/N]: " ink
+	case "$ink" in
+		[yY][eE][sS]|[yY])
+	rm user.cfg;
+	clear
+	sh setup.sh;
+	;;
+	*)
+	echo "Welcome back $uname"
+	;;
+	esac
+fi
+cd
+cd /$HOME
+#update packages
+apt-get update > /dev/null 2>&1 & spinner
+apt-get upgrade -y > /dev/null 2>&1 & spinner
+apt-get autoremove > /dev/null 2>&1 & spinner
+apt-get autoclean > /dev/null 2>&1 & spinner
+apt install figlet toilet -y > /dev/null 2>&1 & spinner
+pkg install ncurses-utils -y > /dev/null 2>&1 & spinner
+apt-get install git -y > /dev/null 2>&1 & spinner
+mkdir -p $PREFIX/var/lib/postgresql > /dev/null 2>&1 & spinner
+cd ../usr/etc > /dev/null 2>&1 & spinner
+rm motd > /dev/null 2>&1 & spinner
+#Set default username if found null
+if [ -z "$uname" ]
+then
+	uname="FemurTech"
+fi
+#Sets bash.bashrc aka startup
+echo "command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "'$1'"
+}
+shell() { 
+	sh \$1.sh;bash \$1.bash;
+ }
+cds() { 
+	if [[ \$* == *\"-i\"* ]]
+	then
+		var=\"\$*\"
+		sub=\"\"
+		qry=\$( echo \$var | sed 's/-i//g' )
+		qry=\$( echo \$qry | sed 's/ //g' )
+		echo finding \\\"\$qry\\\"
+		cd \"\$( find \"\$(pwd)/\" -maxdepth 3 -not \\( -path \"/sdcard/Android/*\" -prune \\) -type d -iname \"\$qry\" -print -quit )\"
+	else
+		echo finding \$1
+		cd \"\$( find \"\$(pwd)/\" -maxdepth 3 -not \\( -path \"/sdcard/Android/*\" -prune \\) -type d -name \"\$1\" -print -quit )\"
+	fi
+}
+cdp() { 
+	if [[ \$* == *\"-i\"* ]]
+	then
+		var=\"\$*\"
+		sub=\"\"
+		qry=\$( echo \$var | sed 's/-i//g' )
+		qry=\$( echo \$qry | sed 's/ //g' )
+		echo finding \\\"\$qry\\\"
+		cd \"\$( find \"\$(pwd)/\" -maxdepth 3 -not \\( -path \"/sdcard/Android/*\" -prune \\) -type d -iname \"\$qry\"* -print -quit )\"
+	else
+		echo finding \$1
+		cd \"\$( find \"\$(pwd)/\" -maxdepth 3 -not \\( -path \"/sdcard/Android/*\" -prune \\) -type d -name \"\$1\"* -print -quit )\"
+	fi
+}
+updatedw() {
+	cd \$HOME
+	if [ -d \"\$HOME/termuxstyling\" ]
+	then
+		cd termuxstyling
+		var=\$(git pull)
+		# echo \$var
+		if [[ \$var == *\"Already\"* ]];
+		then 
+			clear && echo \"Already running the latest version!!\" && echo ------------------------------------- && figlet \$(sed '1q;d' 'ver.cfg')
+		else
+			git pull --rebase && prm sh
+			shell setup;
+			clear && echo \"Update Success\" && echo -------------- && figlet Success && figlet \$(sed '1q;d' 'ver.cfg')
+		fi
+	else
+		git clone https://github.com/darkwarrior3/termuxstyling.git
+		prm sh
+		clear
+		bash
+		shell setup
+		wait
+		clear && echo \"Update Success\" && echo -------------- && figlet Success && figlet \$(sed '1q;d' 'ver.cfg')
+	fi
+}
+prm() { chmod 777 *.\$1; }
+txt() { cat \$1.*; }
+figlet $uname
+PS1='\033[1;91mroot@termux[\033[1;93m\W\033[1;91m]:
+# \033[1;92m'
+if grep -q '# 011' \"/data/data/com.termux/files/home/termuxstyling/ver.cfg\"
+then
+	lnum=\$( sed '2q;d' \"/data/data/com.termux/files/home/termuxstyling/ver.cfg\" )
+	lnum=\$( echo \$lnum | sed 's/# 011//g' )
+	lnum=\$( echo \$lnum | sed 's/ //g' )
+	# echo \$( sed '2q;d' \"/data/data/com.termux/files/home/termuxstyling/ver.cfg\" )
+		if [[ ! \$lnum -eq 25 ]]
+		then
+			lnum=\$((\$lnum+1))
+			sed -i \"/.*# 011.*/ c\\ \$lnum # 011\" \"/data/data/com.termux/files/home/termuxstyling/ver.cfg\"
+		else
+			lnum=1
+			sed -i \"/.*# 011.*/ c\\ \$lnum # 011\" \"/data/data/com.termux/files/home/termuxstyling/ver.cfg\"
+			cd \$HOME
+			cd termuxstyling
+			vtest=\$(git pull)
+			if [[ ! \$vtest == *\"Already\"* ]];
+			then
+				updatedw
+			fi
+		fi
+		
+	else
+		# echo hi
+		echo \"1 # 011\" >> \"/data/data/com.termux/files/home/termuxstyling/ver.cfg\"
+	fi
+cd
+alias md=\"mkdir\"
+alias msf=\"msfconsole\"
+alias msfdb=\"initdb $PREFIX/var/lib/postgresql && pg_ctl -D $PREFIX/var/lib/postgresql start \"
+alias clear=\"clear;bash\"
+alias dir=\"ls\"
+alias ins=\"pkg install\"
+alias ains=\"apt install\"
+alias cls=\"clear\"
+alias update=\"apt-get update && apt-get upgrade\"
+if [ -e \".bash_aliases\" ];then sh ~/../usr/etc/.bash_aliases;fi" > bash.bashrc
+cd /$HOME
+cd termuxstyling
+cat README.md
+toilet Dark
+toilet Warrior
+echo Script made by Dark Warrior
+echo Subscribe to our YT channel FemurTech
+echo tinyurl.com/femurtech
+echo Restart to apply changes
